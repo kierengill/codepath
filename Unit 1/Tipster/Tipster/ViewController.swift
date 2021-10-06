441bd50d6d4ce5c849104c460f79bc6bf0c0f914
Kieren <kierengill2000@gmail.com>
2021-10-06 00:51:08 -0400

    Unit 1

---
+++ Unit 1/Tipster/Tipster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Kieren Gill on 10/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
